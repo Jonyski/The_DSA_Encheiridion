b3a9cb8b7b82f110b2bb516e80fa3ecaa3e76b6a
Jonyski <geleiaeavela@gmail.com>
2024-09-20 11:10:40 -0300

    implements a hash map in C

---
+++ C/hash_map.c
@@ -0,0 +1,146 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// In this file we will implement a
+// simple hash map with chaining to
+// solve collisions. The hash
+// function will map strings keys  
+// using the division method
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//-----------------------------------
+// Imports
+//-----------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+//-----------------------------------
+// Structs, typedefs and defines
+//-----------------------------------
+#define MAP_SIZE 3
+#define NUM_OF_KEYS 10
+#define MAX_KEY_LEN 32
+
+typedef struct {
+	char *key;
+	int value;
+} Element;
+
+typedef struct aux {
+	Element *content;
+	struct aux *next;
+	struct aux *prev;
+} Slot;
+
+//-----------------------------------
+// Function prototypes
+//-----------------------------------
+Slot **create_hashmap();
+char *create_random_key();
+int hash(char *key);
+int set(Slot **map, char *key, Element *el);
+Element *get(Slot **map, char *key);
+int delete(Slot **map, char *key);
+
+
+//-----------------------------------
+// Main
+//-----------------------------------
+int main(int argc, char const *argv[]) {
+	srand(time(NULL));
+	Slot **map = create_hashmap();
+	if(!map) exit(1);
+	char *keys[NUM_OF_KEYS] = { 0 };
+	for(int i = 0; i < NUM_OF_KEYS; i++) {
+		keys[i] = create_random_key();
+		Element *el = malloc(sizeof(Element));
+		if(!el) exit(1);
+		el->value = rand()%1000;
+		el->key = keys[i];
+		set(map, el->key, el);
+		printf("element inserted - key: %s, value: %d\n", el->key, el->value);
+	}
+	printf("\ndeleting random elements\n");
+	for(int i = 0; i < NUM_OF_KEYS; i++) {
+		if(rand()%2) {
+			int result =  delete(map, keys[i]);
+			if(!result) printf("element deleted - key: %s\n", keys[i]);
+		}
+	}
+	printf("\n");
+	for(int i = 0; i < NUM_OF_KEYS; i++) {
+		Element *el = get(map, keys[i]);
+		if(el) printf("element found     - key: %s, value: %d\n", el->key, el->value);
+		else   printf("element not found - key: %s\n", keys[i]);
+	}
+    return 0;
+}
+
+//-----------------------------------
+// Function definitions
+//-----------------------------------
+Slot **create_hashmap() {
+	Slot **map = malloc(sizeof(Slot *) * MAP_SIZE);
+	if(!map) return NULL;
+	for(int i = 0; i < MAP_SIZE; i++) {
+	    map[i] = NULL;
+	}
+	return map;
+}
+
+char *create_random_key() {
+	int key_len = (rand()%(MAX_KEY_LEN)) + 1;
+	char *key = malloc(sizeof(char) * (key_len + 1));
+	key[key_len] = '\0';
+	for(int i = 0; i < key_len; i++)
+		key[i] = (char) 97 + rand()%26;
+	return key;
+}
+
+int hash_key(char *key) {
+	unsigned long long int result = 1;
+	for(int c = 0; c < (int) strlen(key); c++) {
+		result *= (int)key[c];
+	}
+	result = result%MAP_SIZE;
+	return result;
+}
+
+int set(Slot **map, char *key, Element *el) {
+	int index = hash_key(key);
+	Slot *temp = map[index];
+	Slot *slot = malloc(sizeof(Slot));
+	if(!slot) return 1;
+	slot->content = el;
+	slot->next = temp;
+	if(temp) temp->prev = slot;	
+	map[index] = slot;
+	return 0;
+}
+
+Element *get(Slot **map, char *key) {
+	int index = hash_key(key);
+	Slot *curr = map[index];
+	while(curr && strcmp(curr->content->key, key))
+		curr = curr->next;
+	if(!curr) return NULL;
+	return curr->content;
+}
+
+int delete(Slot **map, char *key) {
+	int index = hash_key(key);
+	Slot *curr = map[index];
+	//Slot *prev = NULL;
+	while(curr && strcmp(curr->content->key, key))
+		curr = curr->next;
+	if(!curr) return 1;
+	if(curr->next) curr->next->prev = curr->prev;
+	if(curr->prev) curr->prev->next = curr->next;
+	else {
+		if(!curr->next) map[index] = NULL;
+		else map[index] = curr->next;
+	}
+	free(curr->content);
+	free(curr);
+	return 0;
+}
