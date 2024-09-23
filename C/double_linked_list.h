254c3051d226544f2bbaf9395d3ceba6bd82c7a0
Jonyski <geleiaeavela@gmail.com>
2024-09-23 09:46:54 -0300

    mend

--- C/double_linked_list.h
+++ C/double_linked_list.h
@@ -16,4 +16,13 @@ typedef struct {
 	int len;
 } List;
 
+//-----------------------------------
+// Function prototypes
+//-----------------------------------
+List *create_list();
+int insert_value(List *l, int value);
+int remove_value(List *l, int value);
+void print_list(List *l);
+void free_list(List *l);
+
 #endif
